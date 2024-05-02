082307acc8a61e9061b4457e05be6341f1492a00
Yuliya Zhabina <159014948+Zhabinaj@users.noreply.github.com>
2024-05-02 15:24:21 +0300

    Update session.h
    Добавлен функционал переключения на следующий и предыдущий фрейм

--- session.h
+++ session.h
@@ -24,25 +24,28 @@ private:
     // Status of video condition: PLAY or PAUSE
     PLAYING_STATUS playing_status_;
     std::thread player_;	//поток для плеера
+    bool nextFrameClicked = 0;
 
 public:
     explicit Session(QObject *parent = 0);
     ~Session();
 
     ImageProvider *camera;
-
     ffmpeg *f1;	   //переименовать этот ужас
 
-    void play_thread();
 signals:
     void videoWasOver();
-    void totalFramesChanged(int64_t total_frames_count);
+    void totalFramesChanged(int total_frames_count);
+    void currentFrameChanged(int currentFrame);
 
 public slots:
+    void play_thread();
     void reset();				 // break the loop and clear all buffers
     int open(QUrl url);			 //слот для открытия по адресу
     void playButtonClicked();	 //жмякаем кнопку плэй
     void pauseButtonClicked();
+    void nextFrameButtonClicked();
+    void prevFrameButtonClicked();
 };
 
 #endif	  // SESSION_H
