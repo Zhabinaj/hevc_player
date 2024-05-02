94bb0f44c5ff3900dcf9583682cdbad17ff66715
Yuliya Zhabina <159014948+Zhabinaj@users.noreply.github.com>
2024-05-02 15:25:03 +0300

    Update ffmpeg.cpp
    Добавлен функционал переключения на следующий и предыдущий фрейм

--- ffmpeg.cpp
+++ ffmpeg.cpp
@@ -5,6 +5,8 @@
 #include "ffmpeg.h"
 #include "string.h"
 
+#define AVSEEK_FORCE 0x20000
+
 ffmpeg::ffmpeg(int id_str, QObject *parent) : QObject(parent)
 {
     id_stream = id_str;
@@ -24,34 +26,26 @@ int ffmpeg::initialization(std::string path)
     av_register_all();	  // Регистрация всех доступных форматов и кодеков
 
     const char *filename = path.c_str();	//Имя вашего HEVC файла
-
+    std::cout << "===============Filename: " << filename << " ===============" << std::endl;
+    //===================================================Обработка ошибок: возврат от -1 до -10
     formatContext = avformat_alloc_context();
     if (!formatContext)
-    {
-        // Обработка ошибок
         return -1;
-    }
 
     // Открываем файл
     if (avformat_open_input(&formatContext, filename, nullptr, nullptr) != 0)
-    {
-        // Обработка ошибок
         return -2;
-    }
 
     // Получаем информацию о потоке
     if (avformat_find_stream_info(formatContext, nullptr) < 0)
-    {
-        // Обработка ошибок
         return -3;
-    }
 
     // Находим видео поток
-    int videoStreamIndex = -1;
-    vCodecCtxOrig		 = nullptr;
-    vCodecCtx			 = nullptr;
-    AVCodec *vcodec		 = nullptr;
-    img_convert_context	 = nullptr;
+    videoStreamIndex	= -1;
+    vCodecCtxOrig		= nullptr;
+    vCodecCtx			= nullptr;
+    AVCodec *vcodec		= nullptr;
+    img_convert_context = nullptr;
     std::cout << "nb_streams: " << formatContext->nb_streams << std::endl;
     for (unsigned int i = 0; i < formatContext->nb_streams; i++)	//formatContext->nb_streams = 1
     {
@@ -72,12 +66,9 @@ int ffmpeg::initialization(std::string path)
         }
     }
 
+    // Видео поток не найден
     if (videoStreamIndex == -1)
-    {
-        // Видео поток не найден
         return -5;
-        //asdfsfsf
-    }
 
     img_convert_context = sws_getCachedContext(
         NULL, vCodecCtx->width, vCodecCtx->height, vCodecCtx->pix_fmt,
@@ -101,8 +92,8 @@ int ffmpeg::initialization(std::string path)
     time_base_video = (double)formatContext->streams[id_stream]->time_base.num /
                       (double)formatContext->streams[id_stream]->time_base.den;
 
-    std::cout << "time_base video codec=" << vCodecCtx->time_base.num << "/"
-              << vCodecCtx->time_base.den << " "
+    std::cout << "time_base video codec tbc:" << vCodecCtx->time_base.num << "/"
+              << vCodecCtx->time_base.den << " tbn:"
               << formatContext->streams[id_stream]->time_base.num << "/"
               << formatContext->streams[id_stream]->time_base.den << std::endl;
 
@@ -110,67 +101,65 @@ int ffmpeg::initialization(std::string path)
     AVCodec *codec = avcodec_find_decoder(
         formatContext->streams[videoStreamIndex]->codecpar->codec_id);
 
+    // Декодер не найден
     if (!codec)
-    {
-        // Декодер не найден
         return -6;
-    }
 
     // Создаем контекст кодека
     codecContext = avcodec_alloc_context3(codec);
     if (!codecContext)
-    {
-        // Обработка ошибок
         return -7;
-    }
 
     // Настраиваем контекст кодека
-    if (avcodec_parameters_to_context(
-            codecContext, formatContext->streams[videoStreamIndex]->codecpar) <
-        0)
-    {
-        // Обработка ошибок
+    if (avcodec_parameters_to_context(codecContext, formatContext->streams[videoStreamIndex]->codecpar) < 0)
         return -8;
-    }
 
     // Открываем кодек
     if (avcodec_open2(codecContext, codec, nullptr) < 0)
-    {
-        // Обработка ошибок
         return -9;
-    }
 
     frame = av_frame_alloc();
     avpicture_fill((AVPicture *)frame, vbuffer, AV_PIX_FMT_RGB24,
                    vCodecCtx->width, vCodecCtx->height);
     if (!frame)
-    {
-        // Обработка ошибок
         return -10;
-    }
 
     av_init_packet(&packet);	//AVPacket type this function is deprecated?
 
-    //использовать в пресс плэй?
-    // int frames = 0;
-    // ++frames;
-    // std::cout << "Frames:" << frames << std::endl;
     stream_ = formatContext->streams[videoStreamIndex];	   //видеопоток один, поэтому videoStreamIndex = 0, id_stream = 0 (установлено в момент создания ffmpeg)
 
-    if (stream_->nb_frames > 0)
-        totalFrames = stream_->nb_frames - 1;
-    else
+    double fps = stream_->avg_frame_rate.num / stream_->avg_frame_rate.den;
+    std::cout << "===============FPS: " << fps << " ===============" << std::endl;
+
+    //=================Прогоняем видос, считаем кол-во фреймов
+
+    int ret_read_frame = 0;
+    while (ret_read_frame >= 0)
     {
-
-        //тотал фреймс
-        std::cout << "duration:" << formatContext->duration << std::endl;				   //int
-        std::cout << "avg_frame_rate.num:" << stream_->avg_frame_rate.num << std::endl;	   //int
-        std::cout << "avg_frame_rate.den:" << stream_->avg_frame_rate.den << std::endl;	   //int
-        std::cout << "AV_TIME_BASE:" << AV_TIME_BASE << std::endl;						   //int
-
-        totalFrames = static_cast<unsigned long>(formatContext->duration * (stream_->avg_frame_rate.num / stream_->avg_frame_rate.den) / AV_TIME_BASE);
+        /* функция делит содержимой файла formatContext на фреймы
+        * и возвроащает по одному через packet за каждый вызов
+        * В packet как-то должна хранится ссылка на следующий фрейм
+        */
+        ret_read_frame = av_read_frame(formatContext, &packet);	   //возвращает <0 если конец файла
+        ++totalFrames;
+        // Free the packet that was allocated by av_read_frame
+        av_free_packet(&packet);
     }
-    std::cout << "totalFrames: " << totalFrames << std::endl;
+    --totalFrames;	  //из-за последнего входа в цикл, когда файл пуст
+    //===========================================================
+
+    std::cout << "av_read_frame EOF" << ret_read_frame << std::endl;
+    std::cout << "===============Total Frames: " << totalFrames << " ===============" << std::endl;
+    double seconds = totalFrames / fps;
+    int minutes	   = seconds / 60;
+    seconds		   = seconds - (60 * minutes);
+    int time_ms	   = (totalFrames / fps) * 1000;
+    int duration   = time_ms * 1000;
+
+    std::cout << "===============Time: 00:" << minutes << ":" << seconds << " ===============" << std::endl;
+
+    avio_seek(formatContext->pb, 0, SEEK_SET);	  //при всех 0 сбрасывается на начало видео
+
     play();
     return 0;
 }
@@ -185,6 +174,7 @@ void ffmpeg::resetVideo()
 
 int ffmpeg::play()
 {
+
     // для скорости воспроизведения
     //frame_timer	= av_gettime(); //возвращает текущее время в мс
 
@@ -202,37 +192,38 @@ int ffmpeg::play()
     }
     //==============
 
-    else
+    //  else
+    // {
+    if (packet.stream_index == id_stream)
     {
-        if (packet.stream_index == id_stream)
-        {
-            int frame_finished;
+        int frame_finished;
 
-            /*
+        /*
             *  vCodecCtx - кодек
             *  frame - фрейм packet после декодирования
             *  frame_finished - флаг, не 0 если фрейм декодирован
             *  packet - пакет с данными (недекодированый фрейм), полученными из
             * av_read_frame
             */
-            avcodec_decode_video2(vCodecCtx, frame, &frame_finished, &packet);
-            if (frame_finished)
-            {
-                // Convert the image from its native format to RGB
-                sws_scale(img_convert_context, frame->data, frame->linesize, 0,
-                          vCodecCtx->height, vFrameRGB->data, vFrameRGB->linesize);
+        avcodec_decode_video2(vCodecCtx, frame, &frame_finished, &packet);
+        if (frame_finished)
+        {
+            // Convert the image from its native format to RGB
+            sws_scale(img_convert_context, frame->data, frame->linesize, 0,
+                      vCodecCtx->height, vFrameRGB->data, vFrameRGB->linesize);
 
-                /* Constructs an image - img
+            /* Constructs an image - img
                 * with the given width vCodecCtx->width
                 * height vCodecCtx->height
                 * and format QImage::Format_RGB888 .
                 * that uses an existing memory buffer, data vFrameRGB->data[0]
                 */
-                timg = QImage((uchar *)vFrameRGB->data[0], vCodecCtx->width,
-                              vCodecCtx->height, QImage::Format_RGB888);
+            timg = QImage((uchar *)vFrameRGB->data[0], vCodecCtx->width,
+                          vCodecCtx->height, QImage::Format_RGB888);
 
+            if (setFrameMode == 0)
                 emit signalQImageReady(id_stream, timg);
-                /*
+            /*
                 //=============Скорость воспроизведения видео
                 usleep(20000); //текущий поток приостановлен на 20000мкс либо пока не поступит сигнал по которому вызывается функция обработки сигналов или программа завершает свою работу
 
@@ -250,10 +241,29 @@ int ffmpeg::play()
                 frame_last_pts	 = frame_pts_ms;
                 //======================================
                 */
-            }
         }
-        // Free the packet that was allocated by av_read_frame
-        av_free_packet(&packet);
-        return 1;
     }
+    // Free the packet that was allocated by av_read_frame
+    if (setFrameMode == 0)	  // || (currentFrame+1)!=targetFrame)
+        av_free_packet(&packet);
+    if (setFrameMode == 1 && (currentFrame + 1) != targetFrame)
+        av_free_packet(&packet);
+    return 1;
+    //}
 };
+
+void ffmpeg::setFrame(int targetFrame)
+{
+    setFrameMode = 1;
+    std::cout << "Targer frame: " << targetFrame << std::endl;
+    avio_seek(formatContext->pb, 0, SEEK_SET);
+    currentFrame = 0;
+    while (currentFrame != targetFrame)
+    {
+        play();
+        ++currentFrame;
+    }
+    emit signalQImageReady(id_stream, timg);
+    av_free_packet(&packet);
+    setFrameMode = 0;
+}
